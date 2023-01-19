bab32fbf9a23a86dbb44de81871327f2bdde2dc2
Przemyslaw Labun <labun.przemek@gmail.com>
2023-01-19 02:50:07 +0100

    minor fixes

--- Card_detector_main.py
+++ Card_detector_main.py
@@ -32,8 +32,8 @@ video_stream = Video.Camera_stream((camera_width, camera_height), frames_per_sec
 time.sleep(1)
 
 # load train rank and suit images
-tranin_ranks = Card_detector_functions.load_ranks('Card_Imgs\\')
-train_suits = Card_detector_functions.load_suits('Card_Imgs\\')
+tranin_ranks = Card_detector_functions.load_ranks('Card_Imgs/')
+train_suits = Card_detector_functions.load_suits('Card_Imgs/')
 
 '''
     MAIN LOOP
@@ -65,7 +65,6 @@ while quit_cam == 0:
                 
                 cv2.imshow("Detected card", cards[-1].wrap)
                 '''
-                    preprocess card funct #TODO
                     match card funct #TODO
                     draw result funct #TODO
                 '''
